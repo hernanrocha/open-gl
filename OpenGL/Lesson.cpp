cc1cb09e137cfb7715f56d2d884c104e6530f734
Hernan Rocha <hrocha@pladema.net>
2016-02-21 22:40:04 -0300

    [OpenGL] Lesson 5: 3D Shapes

--- OpenGL/Lesson.cpp
+++ OpenGL/Lesson.cpp
@@ -63,12 +63,33 @@ int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
 	glRotatef(rtri,0.0f,1.0f,0.0f);						// Rotate The Triangle On The Y axis
 
 	glBegin(GL_TRIANGLES);								// Drawing Using Triangles
-		glColor3f(1.0f,0.0f,0.0f);						// Set The Color To Red
-		glVertex3f( 0.0f, 1.0f, 0.0f);					// Top
-		glColor3f(0.0f,1.0f,0.0f);						// Set The Color To Green
-		glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
-		glColor3f(0.0f,0.0f,1.0f);						// Set The Color To Blue
-		glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
+		glColor3f(1.0f,0.0f,0.0f);          // Red
+		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Front)
+		glColor3f(0.0f,1.0f,0.0f);          // Green
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Left Of Triangle (Front)
+		glColor3f(0.0f,0.0f,1.0f);          // Blue
+		glVertex3f( 1.0f,-1.0f, 1.0f);          // Right Of Triangle (Front)
+
+		glColor3f(1.0f,0.0f,0.0f);          // Red
+		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Right)
+		glColor3f(0.0f,0.0f,1.0f);          // Blue
+		glVertex3f( 1.0f,-1.0f, 1.0f);          // Left Of Triangle (Right)
+		glColor3f(0.0f,1.0f,0.0f);          // Green
+		glVertex3f( 1.0f,-1.0f, -1.0f);         // Right Of Triangle (Right)
+
+		glColor3f(1.0f,0.0f,0.0f);          // Red
+		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Back)
+		glColor3f(0.0f,1.0f,0.0f);          // Green
+		glVertex3f( 1.0f,-1.0f, -1.0f);         // Left Of Triangle (Back)
+		glColor3f(0.0f,0.0f,1.0f);          // Blue
+		glVertex3f(-1.0f,-1.0f, -1.0f);         // Right Of Triangle (Back)
+
+		glColor3f(1.0f,0.0f,0.0f);          // Red
+		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Left)
+		glColor3f(0.0f,0.0f,1.0f);          // Blue
+		glVertex3f(-1.0f,-1.0f,-1.0f);          // Left Of Triangle (Left)
+		glColor3f(0.0f,1.0f,0.0f);          // Green
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Right Of Triangle (Left)
 	glEnd();											// Finished Drawing The Triangle
 		
 	glLoadIdentity();									// Reset The Current Modelview Matrix
@@ -76,11 +97,41 @@ int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
 	glRotatef(rquad,1.0f,0.0f,0.0f);					// Rotate The Quad On The X axis
 
 	glBegin(GL_QUADS);									// Draw A Quad
-		glColor3f(0.5f,0.5f,1.0f);						// Set The Color To Blue One Time Only
-        glVertex3f(-1.0f, 1.0f, 0.0f);					// Top Left
-        glVertex3f( 1.0f, 1.0f, 0.0f);					// Top Right
-        glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
-        glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+		glColor3f(0.0f,1.0f,0.0f);          // Set The Color To Green
+		glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Top)
+		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Top)
+		glVertex3f(-1.0f, 1.0f, 1.0f);          // Bottom Left Of The Quad (Top)
+		glVertex3f( 1.0f, 1.0f, 1.0f);          // Bottom Right Of The Quad (Top)
+
+		glColor3f(1.0f,0.5f,0.0f);          // Set The Color To Orange
+		glVertex3f( 1.0f,-1.0f, 1.0f);          // Top Right Of The Quad (Bottom)
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Top Left Of The Quad (Bottom)
+		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Bottom)
+		glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Bottom)
+
+		glColor3f(1.0f,0.0f,0.0f);          // Set The Color To Red
+		glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Front)
+		glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Front)
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Front)
+		glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Front)
+					
+		glColor3f(1.0f,1.0f,0.0f);          // Set The Color To Yellow
+		glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Back)
+		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Back)
+		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Back)
+		glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Back)
+
+		glColor3f(0.0f,0.0f,1.0f);          // Set The Color To Blue
+		glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Left)
+		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Left)
+		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Left)
+		glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Left)
+
+		glColor3f(1.0f,0.0f,1.0f);          // Set The Color To Violet
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Right)
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Right)
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Right)
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Right)
     glEnd();											// Done Drawing The Quad
 
 	rtri+=0.2f;											// Increase The Rotation Variable For The Triangle
