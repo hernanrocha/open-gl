241cb457256353cd83bfbd5561aa0803a27f8c81
Hernan Rocha <hernanrocha93@gmail.com>
2016-04-19 13:26:24 -0300

    [Unity] Proyecto xViewer agregado

---
+++ Unity/Skybox_VR/Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxController : MonoBehaviour {
+
+	public Material[] skyboxes;
+	public int currentSkybox = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.X)) {
+			Debug.Log ("Change skybox");
+			currentSkybox++;
+			if (currentSkybox == skyboxes.Length) {
+				currentSkybox = 0;
+			}
+
+			var skybox = GetComponent<Skybox>();
+			skybox.material = skyboxes [currentSkybox];
+		}
+	}
+}
